0ff469b297f1679fe06d882e2f7fc6ac4b36932f
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2025-03-19 16:23:10 +0545

    Update permissions.ts

--- src/middlewares/permissions.ts
+++ src/middlewares/permissions.ts
@@ -1,23 +1,18 @@
 import { permissionEntity } from "../entity/permissionEntity";
 
-const checkPermission = async (role: string, route: string, action: string) => {
-    const roleData: any = await permissionEntity.findOne({ where: { name: role, route } });
-    console.log(roleData.permissions.includes('add'))
-    return (roleData.permissions.includes(action))
-};
-
 export const permissionMiddleware = (action: string) => {
     return async (req: any, res: any, next: any) => {
         const role = req.user.role
         const route = req.baseUrl.replace(/^\/api\//, "")
 
         if (!role)
-            return res.status(403).json({ message: "User's role not available", success:false });
+            return res.status(403).json({ message: "User's role not available", success: false });
 
-        var hasPermission = await checkPermission(role, route, action)
+        const roleData: any = await permissionEntity.findOne({ where: { name: role, route } });
+        var hasPermission = await roleData.permissions.includes(action)
 
         if (!hasPermission)
-            return res.status(403).json({ message: "Permission Denied", success:false });
+            return res.status(403).json({ message: "Permission Denied", success: false });
 
         next()
 
